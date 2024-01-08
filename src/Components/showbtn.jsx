f3f4eae8954b77299e27171757e19575d014a4d4
swamithedev <swamithedev@gmail.com>
2024-01-08 23:04:43 +0530

    btn component

---
+++ src/Components/showbtn.jsx
@@ -0,0 +1,14 @@
+import style from './showbtn.module.css'
+
+export default function Showbtn({ btnname }) {
+    return (
+        <>
+            <a className={style.fancy} href="#">
+                <span className={style.text} >{btnname}</span >
+                <span className={style.bottom_key_1}  ></span >
+                <span className={style.bottom_key_2}  ></span >
+                <span className={style.top_key} ></span>
+            </a >
+        </>
+    )
+}
