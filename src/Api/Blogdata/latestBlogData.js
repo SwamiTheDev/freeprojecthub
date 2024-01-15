975ae649a779348c68099c2638bc59c797ef8df6
swamithedev <swamithedev@gmail.com>
2024-01-15 20:15:23 +0530

    update lastest blog card

---
+++ src/Api/Blogdata/latestBlogData.js
@@ -0,0 +1,16 @@
+const LastestBlogData = [
+  {
+    id: 1,
+    title: "Mastering CSS Positioning",
+    minicontent:
+      'Css Position property places elements with values like "static," "relative," "absolute," "fixed," or "sticky" within containers.',
+  },
+  {
+    id: 2,
+    title: " CSS Flexbox",
+    minicontent:
+      "Dynamic layout, items in a container adjust size and distribution for flexible, responsive designs",
+  },
+];
+
+export default LastestBlogData;
